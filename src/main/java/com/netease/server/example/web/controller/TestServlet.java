77cac422c8ee51a64f55a64fc69d8ceddf45295e
liusyuan <lsy561061800@163.com>
2016-04-24 15:52:14 +0800

    servlet

--- src/main/java/com/netease/server/example/web/controller/TestServlet.java
+++ src/main/java/com/netease/server/example/web/controller/TestServlet.java
@@ -9,11 +9,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TestServlet extends HttpServlet {
+	
+	@Override
+	public void destroy() {
+		System.out.println("destroy method");
+		super.destroy();
+	}
+	@Override
+	
+	public void init() throws ServletException {
+		System.out.println("init method");
+		super.init();
+	}
 	@Override
 	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
 		System.out.println("service method");
 		super.service(arg0, arg1);
 	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doGet method");
